f3df817ee46dfc6b87bb8f8692ba9325ed91e460
Stefan Pahontu <t-spahontu@microsoft.com>
2022-08-03 15:00:23 +0300

    added chart legend

--- main.js
+++ main.js
@@ -13,12 +13,7 @@ App.main = async function (applicationArguments) {
             [e.flavor]: [...(map[e.flavor] ?? []), e]
         }), {});
 
-        var keys = Object.keys(obj);
-        var newMap = new Map();
-        for (var i = 0; i < keys.length; i++) {
-            newMap.set(keys[i], obj[keys[i]]);
-        }
-        return newMap;
+        return new Map(Object.entries(obj));
 
     }
 
@@ -39,36 +34,96 @@ App.main = async function (applicationArguments) {
         return keys;
     }
 
-    function getLastDaysData(data, numOfDays, keys) {
+    function getLastDaysData(data, numOfDays) {
         var timeDif = 1000 * 60 * 60 * 24 * numOfDays; // ms * s * mins * h * days
         var lastTest = new Date(data[data.length - 1].commitTime);
         var result = data.filter(x => new Date(x.commitTime) >= lastTest - timeDif);
         return result;
     }
 
-    function buildGraph(data, flavors) {
-        const margin = { top: 30, right: 60, bottom: 30, left: 60 },
-            width = 800 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+    function circlePoints(dataGroup, data, color, x, y) {
+        data.forEach(function (point) {
+            dataGroup.append("circle")
+                .attr("fill", color)
+                .attr("r", 3)
+                .attr("cx", x(new Date(point.commitTime)))
+                .attr("cy", y(+point.minTime))
+                .append("title")
+                .text("Exact date: " + point.commitTime + "\n" + "Result: " + +point.minTime + " ms");
 
-        var dataGroup1 = d3.select("body")
-            .append("div");
+        });
+    }
 
-        var dataGroup = dataGroup1.append("svg")
+    function plotVariable(dataGroup, color, data, x, y) {
+        dataGroup.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", color)
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function (d) { return x(new Date(d.commitTime)); })
+                .y(function (d) { return y(+d.minTime); })
+            )
+    }
+
+    function addSimpleText(dataGroup, xCoord, yCoord, textSize, text, color) {
+        return dataGroup.append("g")
+            .append("text")
+            .text(text)
+            .attr("font-size", textSize)
+            .attr("fill", color)
+            .attr("text-anchor", "middle")
+            .attr("x", xCoord)
+            .attr("y", yCoord);
+    }
+
+    function addLegend(dataGroup, xCoord, startY, flavors, colors) {
+        var circleRadius = 5;
+        var textSpacing = 15;
+        var rectWidth = 90;
+        var rectHeight = (flavors.length + 1) * textSpacing;
+        var legend = dataGroup.append("g")
+            .attr("class", "chart-legend");
+        legend.append("rect")
+            .attr("class", "legend")
+            .attr("x", xCoord - 10)
+            .attr("y", startY - 20)
+            .attr("rx", "5px")
+            .attr("width", rectWidth)
+            .attr("height", rectHeight)
+            .attr("stroke", "black")
+            .attr("fill", "white");
+        addSimpleText(legend, xCoord + 35, startY - 10, "7pt", "Chart Legend", "black");
+        for (var i = 0; i < flavors.length; i++) {
+            var yCoord = startY + i * textSpacing;
+            legend.append("circle").attr("cx", xCoord).attr("cy", yCoord).attr("r", circleRadius).style("fill", colors[i]);
+            legend.append("text").text(flavors[i]).attr("font-size", "5pt").attr("fill", "black").attr("text-anchor", "start").attr("x", xCoord + textSpacing).attr("y", yCoord);
+        }
+        return legend;
+    }
+
+    function buildGraph(data, flavors, numOfDays, margin) {
+
+        const width = 800 - margin.left - margin.right;
+        const height = 400 - margin.top - margin.bottom;
+
+        var dataGroup = d3.select("body")
+            .append("div")
+            .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
-        var filteredData = mapByFlavor(data);
-
+        var svg = dataGroup.select("svg");
         var onZoom = d3.zoom().on("zoom", zoomFunction);
         function zoomFunction() {
-            dataGroup.attr("transform", d3.zoomTransform(this));
+            svg.attr("transform", d3.zoomTransform(this));
         }
-        onZoom(dataGroup);
-
+        onZoom(svg);
+        var filteredData = mapByFlavor(data);
         var colors = d3.schemeCategory10;
+
         const x = d3.scaleTime()
             .domain(d3.extent(data, function (d) { return new Date(d.commitTime); }))
             .range([0, width])
@@ -79,46 +134,14 @@ App.main = async function (applicationArguments) {
             .range([height, 0])
             .nice();
 
-        function plotVariable(color, data) {
-            dataGroup.append("path")
-                .datum(data)
-                .attr("fill", "none")
-                .attr("stroke", color)
-                .attr("stroke-width", 1.5)
-                .attr("d", d3.line()
-                    .x(function (d) { return x(new Date(d.commitTime)); })
-                    .y(function (d) { return y(+d.minTime); })
-                )
-        }
-
-        function circlePoints(data, color, flavor) {
-            data.forEach(function (point) {
-                for (var i = 0; i < data.length; i++) {
-                    dataGroup.append("circle")
-                        .attr("fill", color)
-                        .attr("r", 3)
-                        .attr("cx", x(new Date(point.commitTime)))
-                        .attr("cy", y(+point.minTime))
-                        .append("title")
-                        .text("Date: " + d3.timeFormat("%Y-%m-%d")(new Date(point.commitTime)) + "\n" + flavor + ": " + +point.minTime);
-
-                }
-            });
-        }
-
-        for (var i = 0; i < flavors.length; i++) {
-            plotVariable(colors[i], filteredData.get(flavors[i]));
-            circlePoints(filteredData.get(flavors[i]), colors[i], flavors[i]);
-        }
-
-
         var xAxisGroup = dataGroup
             .append("g")
             .attr("class", "xAxisGroup")
             .attr("transform", "translate(0, " + height + ")");
 
         var xAxis = d3.axisBottom(x)
-            .tickFormat(d3.timeFormat("%Y-%m-%d"));
+            .tickFormat(d3.timeFormat("%m/%d/%Y"))
+            .ticks(numOfDays);
 
         xAxis(xAxisGroup);
 
@@ -132,16 +155,15 @@ App.main = async function (applicationArguments) {
         d3.selectAll(".xAxisGroup .tick text")
             .attr("transform", "rotate(-15)");
 
-        var title = dataGroup.append("g");
-        title.append("text")
-            .text(data[0].taskMeasurementName)
-            .attr("font-size", "10pt")
-            .attr("fill", "black")
-            .attr("text-anchor", "middle")
-            .attr("x", width / 2)
-            .attr("y", 10 - (margin.top / 2));
 
-
+        for (var i = 0; i < flavors.length; i++) {
+            plotVariable(dataGroup, colors[i], filteredData.get(flavors[i]), x, y);
+            circlePoints(dataGroup, filteredData.get(flavors[i]), colors[i], x, y);
+        }
+        var title = addSimpleText(dataGroup, width / 2, 10 - (margin.top / 2), "12pt", data[0].taskMeasurementName, "black");
+        var yAxisName = addSimpleText(dataGroup, 0 - margin.top / 2, 0 - (margin.top / 2) + 10, "12pt", "Results (ms)", "black");
+        var startY = (height - flavors.length * 20) / 2;
+        var legend = addLegend(dataGroup, width, startY, flavors, colors);
     }
 
     const exports = await App.MONO.mono_wasm_get_assembly_exports("PerformanceTool.dll");
@@ -152,7 +174,8 @@ App.main = async function (applicationArguments) {
         var flavors = getFlavors(data);
         // 0 for first test, meaning appStart reach managed 
         var firstTry = getWantedTestResults(wantedData, 0);
-        buildGraph(firstTry, flavors);
+        const margin = { top: 60, right: 80, bottom: 80, left: 100 };
+        buildGraph(firstTry, flavors, 14, margin);
 
     });
     await App.MONO.mono_run_main("PerformanceTool.dll", applicationArguments);
